6348f524bd2acb28ef2a004dbc6bf041b601568d
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-04-27 09:20:46 +0200

    initial commit

---
+++ src/test/java/se/iths/imageservice/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
